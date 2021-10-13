2c1cc5694104de3469ce4782f94464ca3b6fc016
Wilsson A. Interiano <71723254+interale14@users.noreply.github.com>
2021-10-12 19:08:25 -0600

    Conexión a la BD
    A MongoDB

---
+++ utils/dao.js
@@ -0,0 +1,31 @@
+var mongoClient = require('mongodb').MongoClient;
+var dotenv = require('dotenv');
+
+dotenv.config();
+
+var {
+    MONGO_USER,
+    MONGO_PSWD,
+    MONGO_HOST,
+    MONGO_DB
+} = process.env;
+
+var connection = null;
+
+var connectionString = `mongodb+srv://${MONGO_USER}:${MONGO_PSWD}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority`;
+
+var client = new mongoClient(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
+
+if (!connection) {
+client.connect()
+    .then( (conn) => {
+        console.log("Conectado");
+        connection = conn;
+    })
+    .catch((err) => {
+        console.log("Error");
+        process.exit(1);
+    } );
+}
+
+module.exports = connection;
